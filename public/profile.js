c659419b4dead071e4246ef5b2df81c0a146331c
John Doe <johndoe@email.com>
2023-05-23 14:52:43 +0300

    vot etot kloniruy

--- public/profile.js
+++ public/profile.js
@@ -1,76 +1,59 @@
 $(function() {
-    $("#navbar").load("navbar.html");
-
-    loadAvatar();
-    loadUserData();
-  });
-  $(document).ready(function() {
-    // Load user data on page load
-    $.ajax({
-      url: 'http://localhost:3000/profile',
-      method: 'GET',
-      success: function(response) {
-        // Update the DOM with the user data
-        $('#name').val(response.name);
-        $('#breed').val(response.breed);
-        $('#description').val(response.description);
-      },
-      error: function() {
-        alert('Error occurred while loading user data');
-      }
-    });
-  
-    // Save user data
-    function saveUserData() {
-      var name = $('#name').val();
-      var breed = $('#breed').val();
-      var description = $('#description').val();
-  
-      // Send the updated data to the server
-      $.ajax({
-        url: 'http://localhost:3000/profile',
-        method: 'POST',
-        data: {
-          name: name,
-          breed: breed,
-          description: description
-        },
-        success: function(response) {
-          alert('User data saved successfully');
-        },
-        error: function() {
-          alert('Error occurred while saving user data');
-        }
-      });
-    }
-  // Function to save the avatar
-function saveAvatar() {
-  // Get the selected avatar file
-  var avatarFile = $('#avatar-input')[0].files[0];
-
-  // Create a new FormData object
-  var formData = new FormData();
-  formData.append('avatar', avatarFile);
-
-  // Send the avatar data to the server
   $.ajax({
-    url: 'http://localhost:3000/avatar',
-    method: 'POST',
-    data: formData,
-    processData: false,
-    contentType: false,
+    url: 'http://localhost:3000/profile',
+    method: 'GET',
     success: function(response) {
-      alert('Avatar saved successfully');
+      $('#name').val(response.name);
+      $('#breed').val(response.breed);
+      $('#description').val(response.description);
     },
     error: function() {
-      alert('Error occurred while saving the avatar');
+      alert('Error occurred while loading user data');
     }
   });
-}
 
-    // Attach event handlers to the save buttons
-    $('#name + .btn-2').click(saveUserData);
-    $('#breed + .btn-2').click(saveUserData);
-    $('#description + .btn-2').click(saveUserData);
-  });
-  
+  function saveUserData() {
+    var name = $('#name').val();
+    var breed = $('#breed').val();
+    var description = $('#description').val();
+
+    $.ajax({
+      url: 'http://localhost:3000/profile',
+      method: 'POST',
+      data: {
+        name: name,
+        breed: breed,
+        description: description
+      },
+      success: function(response) {
+        alert('User data saved successfully');
+      },
+      error: function() {
+        alert('Error occurred while saving user data');
+      }
+    });
+  }
+  function saveAvatar() {
+    var avatarFile = $('#avatar-input')[0].files[0];
+    var formData = new FormData();
+    formData.append('avatar', avatarFile);
+
+    $.ajax({
+      url: 'http://localhost:3000/avatar',
+      method: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(response) {
+        alert('Avatar saved successfully');
+      },
+      error: function() {
+        alert('Error occurred while saving the avatar');
+      }
+    });
+  }
+
+  $(document).on('click', '#name + .btn-2', saveUserData);
+  $(document).on('click', '#breed + .btn-2', saveUserData);
+  $(document).on('click', '#description + .btn-2', saveUserData);
+});
